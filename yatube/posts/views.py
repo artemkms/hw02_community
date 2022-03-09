a6ac95dcd23e58b2177f48269ab84aee72ac81f7
artemkms <69520287+artemkms@users.noreply.github.com>
2022-03-10 01:59:37 +0700

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-
-from .models import Post, Group
+from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
-    return render(request, "index.html", {"posts": posts})
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, group_slug):
-    group = get_object_or_404(Group, slug=group_slug)
-    posts = group.posts.all()[:12]
-    context = {"group": group, "posts": posts}
-    return render(request, "group.html", context)
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
